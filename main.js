ae0dbdcf35667d2a690346c78766d47e51e77322
Caio César <caiocsarbezerra95@gmail.com>
2023-04-04 23:10:29 -0300

    ajuste de botoes
    ajuste dos botoes de navegaçao entre as paginas

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ function novidades(){
     let re = document.getElementById("resident");
     let er = document.getElementById("elden");
 
-    if(re.style.display == "none"){
-        re.style.display = "block"
-        er.style.display = "none"
-    }
-    else if(er.style.display == "none"){
-        er.style.display = "block"
+    if(re.style.display == "grid"){
         re.style.display = "none"
+        er.style.display = "grid"
+    }
+    else if(er.style.display == "grid"){
+        re.style.display = "grid"
+        er.style.display = "none"
 
     }
 }

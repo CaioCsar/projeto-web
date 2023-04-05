8eb960468164d0f239b853b86ff6a16c834b7554
Caio César <caiocsarbezerra95@gmail.com>
2023-04-04 22:44:43 -0300

    resident page
    melhorias e formataçao na pagina do resident

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-function submitForm(){
-    
+function novidades(){
+    let re = document.getElementById("resident");
+    let er = document.getElementById("elden");
+
+    if(re.style.display == "none"){
+        re.style.display = "block"
+        er.style.display = "none"
+    }
+    else if(er.style.display == "none"){
+        er.style.display = "block"
+        re.style.display = "none"
+
+    }
 }

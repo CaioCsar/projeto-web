1732506543bc4e4501914321eb13f96ebb09087e
Caio César <caiocsarbezerra95@gmail.com>
2023-04-06 21:07:41 -0300

    validacao cpf
    fix bug validacao do cpf

--- main.js
+++ main.js
@@ -30,14 +30,14 @@ function TestaCPF() {
         Resto = (Soma * 10) % 11;
       
           if ((Resto == 10) || (Resto == 11))  Resto = 0;
-          if (Resto != parseInt(cpf.value.substring(9, 10)) ) return alert("CPF inválido"), cpf.value = "";
+          if (Resto != parseInt(cpf.value.substring(9, 10)) ) return false;
       
         Soma = 0;
           for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.value.substring(i-1, i)) * (12 - i);
           Resto = (Soma * 10) % 11;
       
           if ((Resto == 10) || (Resto == 11))  Resto = 0;
-          if (Resto != parseInt((cpf.value).substring(10, 11) ) ) return alert("CPF inválido"), cpf.value = "";
+          if (Resto != parseInt((cpf.value).substring(10, 11) ) ) return false
           return true;
     }
 }

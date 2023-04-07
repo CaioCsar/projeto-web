127832970bdb32ff201259802416a7bcba38b661
Caio César <caiocsarbezerra95@gmail.com>
2023-04-06 21:26:35 -0300

    Adicao das paginas de ER e RE a  novidades

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-/* alternar paginas em novidades */
-
-function novidades() {
-    let re = document.getElementById("resident");
-    let er = document.getElementById("elden");
-
-    if (re.style.display == "grid") {
-        re.style.display = "none"
-        er.style.display = "grid"
-    }
-    else if (er.style.display == "grid") {
-        re.style.display = "grid"
-        er.style.display = "none"
-
-    }
-}
 
 /* VALIDA CPF */
 
